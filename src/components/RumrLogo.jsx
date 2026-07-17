08f6968ffd93583df0e65b0b53a654bc7f318baf
RUMR Studios <rumrstudios@gmail.com>
2026-07-17 16:20:41 +0530

    design: correct SVG paths for RUMR logo

--- src/components/RumrLogo.jsx
+++ src/components/RumrLogo.jsx
@@ -12,19 +12,22 @@ export default function RumrLogo({ className = "", height = 36, fill = "currentC
       style={{ display: "inline-block", verticalAlign: "middle" }}
     >
       {/* Letter R */}
+      {/* Left stem */}
+      <rect x="0" y="0" width="5.5" height="60" fill={fill} />
+      {/* Top loop */}
       <path
-        d="M 0,0 L 14,0 C 19.5,0 22,3.5 22,13.5 C 22,23.5 19.5,27 14,27 L 5.5,27 L 5.5,60 L 0,60 Z M 5.5,5.5 L 13.5,5.5 C 16.5,5.5 16.5,7.5 16.5,13.5 C 16.5,19.5 16.5,21.5 13.5,21.5 L 5.5,21.5 Z"
+        d="M 5.5,0 L 14,0 C 19.5,0 22,3.5 22,13.5 C 22,23.5 19.5,27 14,27 L 5.5,27 Z M 5.5,5.5 L 13.5,5.5 C 16.5,5.5 16.5,7.5 16.5,13.5 C 16.5,19.5 16.5,21.5 13.5,21.5 L 5.5,21.5 Z"
         fill={fill}
       />
-      <path
-        d="M 10,27 L 16.5,27 L 23.5,60 L 17,60 Z"
-        fill={fill}
-      />
+      {/* Vertical right leg */}
+      <rect x="16.5" y="27" width="5.5" height="33" fill={fill} />
 
       {/* Middle Glyph U/x/C */}
-      {/* Top Arch */}
+      {/* Top U shape (open at top) */}
+      <rect x="30" y="4" width="5.5" height="12" fill={fill} />
+      <rect x="46.5" y="4" width="5.5" height="12" fill={fill} />
       <path
-        d="M 30,16 C 30,4 52,4 52,16 L 46.5,16 C 46.5,9.5 35.5,9.5 35.5,16 Z"
+        d="M 30,16 C 30,22 52,22 52,16 L 46.5,16 C 46.5,18 35.5,18 35.5,16 Z"
         fill={fill}
       />
       {/* Cross X */}
@@ -36,9 +39,11 @@ export default function RumrLogo({ className = "", height = 36, fill = "currentC
         d="M 48.5,22 L 33.5,35 L 37.5,38 L 52.5,25 Z"
         fill={fill}
       />
-      {/* Bottom Arch */}
+      {/* Bottom inverted U shape (open at bottom) */}
+      <rect x="30" y="38" width="5.5" height="12" fill={fill} />
+      <rect x="46.5" y="38" width="5.5" height="12" fill={fill} />
       <path
-        d="M 30,41 C 30,53 52,53 52,41 L 46.5,41 C 46.5,47.5 35.5,47.5 35.5,41 Z"
+        d="M 30,38 C 30,32 52,32 52,38 L 46.5,38 C 46.5,36 35.5,36 35.5,38 Z"
         fill={fill}
       />
       {/* STUDIOS Text */}
@@ -59,19 +64,24 @@ export default function RumrLogo({ className = "", height = 36, fill = "currentC
       <rect x="60" y="0" width="5.5" height="60" fill={fill} />
       <rect x="76.5" y="0" width="5.5" height="60" fill={fill} />
       <path
-        d="M 65.5,0 L 71,28 L 71,28 L 76.5,0 L 82,0 L 73.75,34 L 68.25,34 L 60,0 Z"
+        d="M 65.5,0 L 73.75,60 L 68.25,60 L 60,0 Z"
+        fill={fill}
+      />
+      <path
+        d="M 76.5,0 L 68.25,60 L 73.75,60 L 82,0 Z"
         fill={fill}
       />
 
       {/* Letter Я (Backwards R) */}
+      {/* Right stem */}
+      <rect x="106.5" y="0" width="5.5" height="60" fill={fill} />
+      {/* Top loop */}
       <path
-        d="M 112,0 L 98,0 C 92.5,0 90,3.5 90,13.5 C 90,23.5 92.5,27 98,27 L 106.5,27 L 106.5,60 L 112,60 Z M 106.5,5.5 L 98.5,5.5 C 95.5,5.5 95.5,7.5 95.5,13.5 C 95.5,19.5 95.5,21.5 98.5,21.5 L 106.5,21.5 Z"
+        d="M 112,0 L 98,0 C 92.5,0 90,3.5 90,13.5 C 90,23.5 92.5,27 98,27 L 106.5,27 Z M 106.5,5.5 L 98.5,5.5 C 95.5,5.5 95.5,7.5 95.5,13.5 C 95.5,19.5 95.5,21.5 98.5,21.5 L 106.5,21.5 Z"
         fill={fill}
       />
-      <path
-        d="M 102,27 L 95.5,27 L 88.5,60 L 95,60 Z"
-        fill={fill}
-      />
+      {/* Vertical left leg */}
+      <rect x="90" y="27" width="5.5" height="33" fill={fill} />
 
       {/* Dot . */}
       <circle cx="122.5" cy="57" r="3.2" fill={fill} />
